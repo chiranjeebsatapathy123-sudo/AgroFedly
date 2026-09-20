92e9e34f11a2daf35d08e0f2d52a209bcb70bf75
HackNova <chiranjeebsatapathy123@gmail.com>
2026-09-20 20:01:02 +0530

    Fix session dropping issue by removing signed_cookies session engine

--- AgroFedly/settings.py
+++ AgroFedly/settings.py
@@ -180,8 +180,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "dashboard"
 LOGOUT_REDIRECT_URL = "login"
-SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
-
 WEATHER_API_KEY = os.getenv("WEATHER_API_KEY", "")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
 API_KEY = os.getenv("API_KEY")
